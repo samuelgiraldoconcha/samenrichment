4a106569593e4224f3cb4c9467e962922cf5ab40
Samuel Detox <samuelgiraldoconcha@gmail.com>
2024-10-03 14:40:01 -0500

    Corrected wrong commas

--- models.py
+++ models.py
@@ -3,27 +3,26 @@ import pydantic
 from pydantic import Basemodel, PositiveInt
 
 class Prospect(Basemodel):
-    id: int,
-    vc_firm: str,
-    startup: str,
-    additional_info: str,
-    enriched: bool,
+    id: int
+    vc_firm: str
+    startup: str
+    additional_info: str
+    enriched: bool
     enrichment_data: dict[str,str]
 
 class Enrichment_target(Basemodel):
-    id: int,
-    name: str,
-    main_link: str,
-    scrape_link: str,
+    id: int
+    name: str
+    main_link: str
+    scrape_link: str
     scrape_fields: dict[str, str]
 
 class Enrichment(Basemodel):
-    id: int,
-    date: str,
-    prospects: List[Prospect],
-    enrichment_targets: List[Enrichment_target],
+    id: int
+    date: str
+    prospects: List[Prospect]
+    enrichment_targets: List[Enrichment_target]
     enriched_data: dict[Prospect, dict[str,str]]
-    
-    def 
+
 
 print(pydantic.__version__)
